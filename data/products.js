ee62ada813ff033648e0afc1165a64385c05d4ee
AndreiSusha <Andrey.Susha@gmail.com>
2024-04-08 20:35:37 +0300

    feat: first commit

---
+++ data/products.js
@@ -0,0 +1,90 @@
+const products = [
+  {
+    id: 0,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1667682942060-977925f9a54b?q=80&w=3972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Pepperoni with pepper",
+    price: 8,
+    description: "A classic pepperoni pizza with an extra kick of pepper.",
+  },
+  {
+    id: 1,
+    imageUrl:
+      "https://images.unsplash.com/photo-1632428442713-1f13a6c56ec4?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "4 Cheeses",
+    price: 7,
+    description: "A delightful blend of four different types of cheeses.",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://images.unsplash.com/photo-1592977731761-2d58aafef572?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Barbecue Chicken",
+    price: 15,
+    description: "A tangy barbecue chicken pizza with a hint of sweetness.",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://images.unsplash.com/photo-1566272794042-adbfe3ca6695?q=80&w=3851&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Sweet and sour chicken",
+    price: 20,
+    description:
+      "A unique combination of sweet and sour flavors with tender chicken pieces.",
+  },
+  {
+    id: 4,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1679924471066-dd984e92f395?q=80&w=3928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "With fresh tomatoes and basil",
+    price: 15,
+    description:
+      "A refreshing pizza topped with fresh tomatoes and basil leaves.",
+  },
+  {
+    id: 5,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1674147605295-53b30e11d8c0?q=80&w=3987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Crazy pepperoni",
+    price: 8,
+    description:
+      "A wild variation of pepperoni pizza for those who like it spicy.",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1667682942099-4d38cc8cc091?q=80&w=3987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Pepperoni classic",
+    price: 15,
+    description: "The timeless classic pepperoni pizza loved by everyone.",
+  },
+  {
+    id: 7,
+    imageUrl:
+      "https://images.unsplash.com/photo-1608562099373-69dd6b715cd9?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Margherita",
+    price: 12,
+    description:
+      "A simple yet delicious pizza with fresh tomatoes, mozzarella, and basil.",
+  },
+  {
+    id: 8,
+    imageUrl:
+      "https://images.unsplash.com/photo-1642789736327-14d9c36496d7?q=80&w=3613&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Four seasons",
+    price: 11,
+    description:
+      "Experience the flavors of all four seasons with this unique pizza.",
+  },
+  {
+    id: 9,
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1663858366999-aa1ce123a972?q=80&w=3600&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "With mushrooms",
+    price: 9,
+    description:
+      "A savory pizza loaded with flavorful mushrooms for a hearty meal.",
+  },
+];
+
+module.exports = products;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-// now we can get json data from th eclient
+const products = require("./data/products");
+// now we can get json data from the client
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -11,82 +12,70 @@ const logTime = (req, res, next) => {
 };
 app.use(logTime);
 
-// IRL we could connect to the database
-// But here we will use array of JS objects
-
-let products = [
-  { id: 1, name: 'chair', price: 140 },
-  { id: 2, name: 'table', price: 200 },
-  { id: 3, name: 'closet', price: 400 },
-];
-
 // Route to get all the products
 // localhost:8000 /api/products GET method
-app.get('/api/products', (req, res) => {
-  res.json(products);
+app.get("/api/products/", (req, res) => {
+  res.status(200).json(products);
 });
 
 // Set the port
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// Get on product based on ID
+// GET
 // localhost:8000 /api/products/3
 
-app.get('/api/products/:id', (req, res) => {
+app.get("/api/products/:id", (req, res) => {
   const productId = Number(req.params.id);
 
-  // const car = cars.find(car => car.id == id)
   const product = products.find((product) => product.id === productId);
 
   if (product) {
     res.status(200).json(product);
   } else {
     res.status(404).json({
-      msg: 'Not found',
+      msg: "Not found",
     });
   }
 });
 
 // DELETE
-app.delete('/api/products/:id', (req, res) => {
+app.delete("/api/products/:id", (req, res) => {
   const id = Number(req.params.id);
   const product = products.find((product) => product.id == id);
   if (product == undefined) {
-    res.status(404).json({ message: 'Product not found' });
+    res.status(404).json({ message: "Product not found" });
     return;
   } else {
     products = products.filter((product) => product.id != id);
-    res.status(200).json({ message: 'Product deleted' });
+    res.status(200).json({ message: "Product deleted" });
   }
 });
 
-// Create
-app.post('/api/products', (req, res) => {
-  //   console.log(req.body);
-  //   res.send('Ok');
+// CREATE
+app.post("/api/products", (req, res) => {
   const lastId = products[products.length - 1].id;
   const newId = lastId + 1;
 
-  //res.send('Ok');
-
   newProduct = {
     id: newId,
-    namme: req.body.name,
+    name: req.body.name,
     price: req.body.price,
+    imageUrl: req.body.imageUrl,
+    description: req.body.description,
   };
 
   products.push(newProduct);
 
   res
     .status(201)
-    .json({ msg: 'The product hase been added successfully!', newProduct });
-  res.location('http://localhost:8000/api/products/' + newId);
+    .json({ msg: "The product has been added successfully!", newProduct });
+  res.location("http://localhost:8000/api/products/" + newId);
   res.status(201).json(newProduct);
 });
 
-// Update
-app.patch('/api/products/:id', (req, res) => {
+// UPDATE
+app.patch("/api/products/:id", (req, res) => {
   const idToUpdate = Number(req.params.id);
   const newName = req.body.name;
   const newPrice = req.body.price;
@@ -102,6 +91,6 @@ app.patch('/api/products/:id', (req, res) => {
   if (product) {
     res.status(200).json(product);
   } else {
-    res.status(404).json({ msg: 'Could not find the product' });
+    res.status(404).json({ msg: "Could not find the product" });
   }
 });
